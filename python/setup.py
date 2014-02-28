1cb0ee1be041b48d9526642e699ddece9c8cf40d
Chongyu Zhu <i@lembacon.com>
2014-02-28 22:31:24 +0800

    Bump to 0.2.7

--- python/setup.py
+++ python/setup.py
@@ -33,7 +33,7 @@ DOUAS_MODULE = Extension('douas',
 
 setup(
     name='douas',
-    version='0.2.6',
+    version='0.2.7',
     description='A Core Audio based streaming audio player for iOS/Mac',
     url='https://github.com/douban/DOUAudioStreamer',
     ext_modules=[DOUAS_MODULE],
